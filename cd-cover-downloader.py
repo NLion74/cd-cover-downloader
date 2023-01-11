327b014dd3a9038dd94679ec5f2a083e6eaccafb
NLion74 <97677226+NLion74@users.noreply.github.com>
2023-01-11 17:22:21 +0100

    Initial Commit

---
+++ cd-cover-downloader.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+import requests
+import discogs_client
+
+d = discogs_client.Client('Mozilla/5.0', user_token='your_user_token') # You will have to create an discogs account and go to this link: https://www.discogs.com/de/settings/developers
+
+path = 'your_cd_directory'
+list_ = os.listdir(path)
+failures = []
+
+for dir_ in list_:
+    if os.path.isdir(path + '/' + dir_):
+        album = dir_
+        print(album)
+        try:
+            results = d.search(album)
+            result = results.page(0)[1]
+
+            images = result.images
+            image = images[0]
+            imageurl = image['uri']
+            ext = imageurl.rsplit(".", 1)[1]
+        except:
+            print("An exception error for occurred")
+            failures.append(album)
+            continue
+
+        imageres = requests.get(imageurl, stream=True, headers={"User-Agent": 'Mozilla/5.0'})
+
+        if imageres.status_code == 200:
+            with open(path + "/" + dir_ + "/cover." + ext, 'wb') as f:
+                shutil.copyfileobj(imageres.raw, f)
+            print("Album Cover Successfully downloaded")
+        else:
+            print("Album Cover failed to download")
+            failures.append(album)
+
+    if os.path.isfile(path + "/" + dir_):
+        continue
+
+if failures:
+    print("The Following Albums could not be processed, please verify that their names are correct or download the covers manually")
+    for album in failures:
+        print(album)
+if not failures:
+    print("All Covers have been downloaded successfully")
