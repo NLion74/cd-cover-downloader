c6abba6622fb8e924b175c4dd8d0ada6f8d6a59b
NLion74 <97677226+NLion74@users.noreply.github.com>
2023-01-13 18:03:10 +0100

    Added force skip option
    Added option to create a file called "force_skip" in album folder to skip the folder

--- cd-cover-downloader.py
+++ cd-cover-downloader.py
@@ -3,17 +3,30 @@ import shutil
 import requests
 import discogs_client
 
-d = discogs_client.Client('Mozilla/5.0', user_token='your_user_token') # You will have to create an discogs account and go to this link: https://www.discogs.com/de/settings/developers
+d = discogs_client.Client('Mozilla/5.0',
+                          user_token='your_user_token')  # You will have to create an discogs account and go to this link: https://www.discogs.com/de/settings/developers
 
 path = 'your_cd_directory'
 list_ = os.listdir(path)
 total = len(list_)
 failures = []
+skips = []
 counter = 0
 
+
 def fetchcover(dir, counter):
-    if os.path.isdir(path + '/' + dir):
-        album = dir
+    album = dir
+
+    if os.path.exists(path + '/' + dir + '/force_skip'):
+        print(album + " (" + str(counter) + "/" + str(total) + ")")
+        print("Album force skipped")
+        skips.append(album)
+        return False
+
+    elif os.path.isfile(path + "/" + dir):
+        return False
+
+    elif os.path.isdir(path + '/' + dir):
         try:
             results = d.search(album, type="release")
             result = results.page(0)[1]
@@ -39,23 +52,28 @@ def fetchcover(dir, counter):
             print("Album Cover failed to download")
             failures.append(album)
 
-    if os.path.isfile(path + "/" + dir):
-        return False
 
 for dir_ in list_:
     counter += 1
     fetchcover(dir_, counter)
 
-if len(failures) == 1:
+if failures:
     print("")
-    print("The Following Album could not be processed. Please verify their names are correct or consider downloading them manually.")
+    print("The Following Album(s) could not be processed: ")
     for album in failures:
         print(album)
-elif failures:
-    print("")
-    print("The Following " + str(len(failures)) + " Albums could not be processed. Please verify their names are correct or consider downloading them manually.")
-    for album in failures:
-        print(album)
+
+    if skips:
+        print("")
+        print("The Following Album(s) were force skipped: ")
+        for album in skips:
+            print(album)
 if not failures:
     print("")
-    print("All Covers have been downloaded successfully.")
+    print("All Album(s) were successfully processed")
+
+    if skips:
+        print("")
+        print("The Following Album(s) were force skipped: ")
+        for album in skips:
+            print(album)
