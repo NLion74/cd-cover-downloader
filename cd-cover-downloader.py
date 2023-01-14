d7f82b19dfdd1f292b6f8ff4a8749a8981cc0068
NLion74 <97677226+NLion74@users.noreply.github.com>
2023-01-14 20:21:57 +0100

    Implemented Cache
    Implemented Cache because there is no need to always redownload the covers and this way they will only be downloaded once. The -r option can be used to clear the cache.

--- cd-cover-downloader.py
+++ cd-cover-downloader.py
@@ -1,11 +1,13 @@
 import os
 import shutil
+import sys
 import requests
 import discogs_client
+import pickle
 
 d = discogs_client.Client('Mozilla/5.0', user_token='your_user_token')  # You will have to create an discogs account and go to this link: https://www.discogs.com/de/settings/developers
 
-path = 'your_cd_directory'
+path = 'your_album_directory'
 list_ = os.listdir(path)
 total = len(list_)
 failures = []
@@ -15,6 +17,11 @@ counter = 0
 
 def fetchcover(dir, counter):
     album = dir
+
+    if album in processed:
+        print(album + " (" + str(counter) + "/" + str(total) + ")")
+        print("Album already processed")
+        return False
 
     if os.path.exists(path + '/' + dir + '/force_skip'):
         print(album + " (" + str(counter) + "/" + str(total) + ")")
@@ -46,15 +53,38 @@ def fetchcover(dir, counter):
                 shutil.copyfileobj(imageres.raw, f)
             print(album + " (" + str(counter) + "/" + str(total) + ")")
             print("Album Cover Successfully downloaded")
+            processed.append(album)
         else:
             print(album + " (" + str(counter) + "/" + str(total) + ")")
             print("Album Cover failed to download")
             failures.append(album)
 
 
+if len(sys.argv) == 2:
+    if sys.argv[1] == "-r" or sys.argv[1] == "--refresh":
+        processed = []
+
+    elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
+        print("Options: ")
+        print("  -r, --refresh      Clearing the processed cache and therefore refreshing all covers")
+        print("  -h, --help         The option used to show this menu")
+        exit()
+elif len(sys.argv) > 2:
+    print("You can only pass one argument at a time. Use --help to list all available options")
+    exit()
+else:
+    if os.path.exists("processed.pickle"):
+        with open("processed.pickle", "rb") as f:
+            processed = pickle.load(f)
+    else:
+        processed = []
+
 for dir_ in list_:
     counter += 1
     fetchcover(dir_, counter)
+
+with open("processed.pickle", 'wb') as f:
+    pickle.dump(processed, f)
 
 if failures:
     print("")
